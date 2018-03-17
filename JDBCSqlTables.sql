c59bba3d4a640d57a34ff389649d1a7bdf0db751
Alec <32371145+zance1054@users.noreply.github.com>
2018-03-16 19:10:06 -0700

    Update JDBCSqlTables.sql

--- JDBCSqlTables.sql
+++ JDBCSqlTables.sql
@@ -8,16 +8,16 @@ CREATE TABLE writingGroup
 (
   groupName  VARCHAR(20)    NOT NULL,
   headWriter VARCHAR(20)    NOT NULL,
-  yearFormed INTEGER        NOT NULL,
+  yearFormed VARCHAR(5)       NOT NULL,
   subject    VARCHAR(20)    NOT NULL,
-  CONSTRAINT pk_writingGroup PRIMARY KEY (GroupName)
+  CONSTRAINT pk_writingGroup PRIMARY KEY (groupName)
 );
 
 CREATE TABLE book
 (
   bookTitle VARCHAR(20) NOT NULL,
-  yearPublished INTEGER NOT NULL,
-  numberPages   INTEGER NOT NULL,
+  yearPublished VARCHAR(5) NULL,
+  numberPages   VARCHAR(5) NULL,
   groupName  VARCHAR(20)    NOT NULL,       /* Foreign Key */
   punlisherName VARCHAR(20) NOT NULL,             /* Foreign Key */
   CONSTRAINT pk_book PRIMARY KEY (GroupName, BookTitle)
@@ -27,7 +27,16 @@ CREATE TABLE publishers
 (
   publisherName VARCHAR(20) NOT NULL,
   pubAddress  VARCHAR(20) NOT NULL,
-  publisherPhone    VARCHAR(11) NOT NULL,
+  publisherPhone    VARCHAR(20) NOT NULL,
   pubEmail    VARCHAR(20) NOT NULL,
   CONSTRAINT pk_publishers PRIMARY KEY (PublisherName)
 );
+
+INSERT INTO writingGroup VALUES('Gryffindors', 'Harry', '2017', 'Fantasy');
+INSERT INTO writingGroup VALUES('writingLife', 'Tom', '2014', 'Drama');
+
+INSERT INTO publishers VALUES('Steve Smith', '123 Rose St.', '555-555-5555', 'steve@gmail.com');
+
+DROP TABLE writingGroup;
+DROP TABLE book;
+DROP TABLE publishers;
